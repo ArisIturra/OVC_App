d9be5aa84144d86349dc08a69d91902fcb376ffb
ppalma <mail@ppalma.cl>
2012-03-23 16:07:33 -0400

    HotFixes

--- logbookShifts/models.py
+++ logbookShifts/models.py
@@ -3,48 +3,38 @@ from django.forms import ModelForm
 from django.contrib.localflavor.us.models import PhoneNumberField
 
 WAYCALL_CHOICES=(('Ph','Phone'),('EM','Mail'),)
-STATUS_CHOICE=((1,'open'),(2,'close'),)
+STATUS_CHOICE=(('1','open'),('2','close'),)
 
 class Mail(models.Model):
 	r_email = models.EmailField('E-Mail requester')
 
+	def __unicode__(self):
+		return u'%s'%(self.r_email)
+
 class Phone(models.Model):
 	r_phone= PhoneNumberField('Phone requester')
 
+	def __unicode__(self):
+		return u'%s'%(self.r_phone)
 class Requester(models.Model):
 	requester = models.CharField('Requester',max_length=200)
 
+	def __unicode__(self):
+		return u'%s'%(self.requester)
+
+
+
 class Logbook(models.Model):
-#	WayCall = models.CharField('Way Call requester',max_length=2,choices=WAYCALL_CHOICES)
-	mail = models.ForeignKey(Mail)
-	phone = models.ForeignKey(Phone)
+	mail = models.ForeignKey(Mail,blank=True,null=True)
+	phone = models.ForeignKey(Phone,blank=True,null=True)
 	b_request = models.DateTimeField('Begin request')
 	e_request = models.DateTimeField('End request')
 	requester = models.ForeignKey(Requester)
-	r_solution= models.TextField('Requeste Solution') 
-    
-#	R_Status = models.CharField('Status',max_length=2,choices=STATUS_CHOICE, default=1)
-###    
-#    def was_published_today(self):
-#        return self.B_Date.date() == datetime.date.today()
+	request	= models.TextField('Request') 
+	solution= models.TextField('Requeste Solution') 
+   	status = models.CharField('Status',max_length=1,choices=STATUS_CHOICE, default=1)	
 
-#    def __str__(self):
-#        return self.WayCall
+	def __unicode__(self):
+		return u'%s %s'%(self.b_request,self.requester)
 
-
-
-#
-
-#
-
-
-
-
-
-
-
-
-      
-#class LogbookForm(ModelForm):
-#    class Meta:
-#        model = Logbook      
+ 
