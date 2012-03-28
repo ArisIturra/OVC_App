ede203a206d4fecaba3278bec9d1707375dfe39c
ppalma <mail@ppalma.cl>
2012-03-28 16:00:29 -0400

    fixed index

--- urls.py
+++ urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.views.generic.simple import redirect_to
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -14,6 +15,7 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
+	(r'^$',redirect_to, {'url': '/admin'}),
      url(r'^admin/', include(admin.site.urls)),
 
 )
