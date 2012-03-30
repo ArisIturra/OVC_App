404ecee1e60c8ebff9e4728c16dd138844fd6bdb
ppalma <mail@ppalma.cl>
2012-03-30 16:01:44 -0400

    end of day

--- Aditional_Rest/admin.py
+++ Aditional_Rest/admin.py
@@ -41,13 +41,13 @@ class RecessRequestAdmin(admin.ModelAdmin):
 
     	def queryset(self, request):
         	"""Limit Pages to those that belong to the request's user."""
+
         	qs = super(RecessRequestAdmin, self).queryset(request)
+
         	if request.user.is_superuser:
-            # It is mine, all mine. Just return everything.
             		return qs
-        # Now we just add an extra filter on the queryset and
-        # we're done. Assumption: Page.owner is a foreignkey
-        # to a User.
+
+
 		ruser = RutUser.objects.get(user=request.user)
         	return qs.filter(user=ruser)
 
