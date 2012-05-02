b588871fc85000eaaf4714e57b729046b220b99c
ppalma <mail@ppalma.cl>
2012-05-02 15:03:44 -0400

    comiting to checkout

--- logbookShifts/admin.py
+++ logbookShifts/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 
 
 class LogbookAdmin(admin.ModelAdmin):
-        list_display = ('b_date','b_time','e_time','delta_time','request')
+        list_display = ('b_date','b_time','e_time','delta_time','request','solution')
         date_hierarchy = ('b_request')
 
 admin.site.register(Logbook,LogbookAdmin)
