1f800fc4bf8bde346110fddc8edbb85e4feac055
ppalma <mail@ppalma.cl>
2012-05-02 12:19:49 -0400

    u know

--- logbookShifts/admin.py
+++ logbookShifts/admin.py
@@ -2,7 +2,11 @@ from OVC_App.logbookShifts.models import Requester, Logbook, Phone, Mail
 from django.contrib import admin
 
 
-admin.site.register(Logbook)
+class LogbookAdmin(admin.ModelAdmin):
+        list_display = ('b_date','b_time','e_time','delta_time','request')
+        date_hierarchy = ('b_request')
+
+admin.site.register(Logbook,LogbookAdmin)
 admin.site.register(Requester)
 admin.site.register(Phone)
 admin.site.register(Mail)
