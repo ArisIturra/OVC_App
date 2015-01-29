b5823a8e872c08e9abd267516de1586a23a2eef3
Patricio Palma <mail@ppalma.cl>
2015-01-29 12:56:48 -0300

    Alpha located last 10 quakes

--- activities/views.py
+++ activities/views.py
@@ -1,6 +1,11 @@
 # Create your views here.
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
+from django.db import connection
+
+from  seismicity.models import *
 
 def index(request):
+	quakes = Seism.objects.all().order_by('-id')[:10]
+	print quakes
 	return render_to_response('activity.html',locals())
